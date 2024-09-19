7871eadd854468d56d694976848a9d0685cd09c5
missil117 <ezequielmanfredi46@gmail.com>
2024-09-19 10:50:37 -0300

    solucion products service

--- src/app/services/productService.tsx
+++ src/app/services/productService.tsx
@@ -1,18 +1,26 @@
 import { IProduct } from "@/interfaces/product";
 
-export const getProductService = async (url: string) => {
-    const response = await fetch(url, { next: { revalidate: 0}});
-    const products = await response.json();
+export const getProductService = async (url: string): Promise<IProduct[]> => {
+    try {
+        const response = await fetch(url, { next: { revalidate: 0 } });
 
-return products; 
-};
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error(`HTTP error! Status: ${response.status}, Response: ${errorText}`);
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-export const getProductById = async (url: string, id: string) => {
-const response = await getProductService(url);
+        const products = await response.json();
 
-const product = response.filter(
-    (item: IProduct) => item.id.toString() === id
-)[0];
-    
-    return product;
+        // Asegúrate de que la respuesta sea un array de productos
+        if (!Array.isArray(products)) {
+            console.error('Invalid data format: Expected an array');
+            throw new Error('Invalid data format: Expected an array');
+        }
+
+        return products;
+    } catch (error) {
+        console.error('Error in getProductService:', error);
+        throw error; // Propaga el error para que pueda ser manejado por el componente
+    }
 };

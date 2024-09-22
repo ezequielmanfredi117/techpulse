464281bd727df7c3cf12fd9ac81fb44582c72d94
missil117 <ezequielmanfredi46@gmail.com>
2024-09-22 18:47:40 -0300

    products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,23 @@
+import ProductCard from '@/components/productCard';
+import { IProduct } from '@/interfaces/product';
 import React from 'react';
-import ProductList from '../../components/productList';
+import { getProductService } from '../services/productService';
 
 
-const ProductsPage = () => {
+
+const ProductsPage = async () => {
+
+  const url = `${process.env.API_URL}/products`;
+  const products = await getProductService(url);
+
 return (
     <div className="container mx-auto p-4 ">
       <h1 className="text-4xl font-bold mb-8 flex items-center justify-center text-blue-950">Products</h1>
-      <ProductList />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {products.map((product: IProduct, i: number) => (
+        <ProductCard key={i} product={product} />
+      ))}
+    </div>
     </div>
   );
 };

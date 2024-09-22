7cee95eb3b5b9ac73a8867ac39af2856d0a85484
missil117 <ezequielmanfredi46@gmail.com>
2024-09-22 17:37:46 -0300

    s2

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProductList from '../../components/productList';
 
 
-const ProductsPage: React.FC = () => {
+const ProductsPage = () => {
 return (
     <div className="container mx-auto p-4 ">
       <h1 className="text-4xl font-bold mb-8 flex items-center justify-center text-blue-950">Products</h1>
